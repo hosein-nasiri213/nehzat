a923683bf3829e83a69fedb8159f75a50d4e54cb
hosein-nasiri213 <hoseinmnasiri213@gmail.com>
2023-06-19 14:12:01 +0330

    header

---
+++ index.js
@@ -0,0 +1,65 @@
+//slider bala 3model function
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     autoplay: {
+//       delay: 5000,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     scrollbar: {
+//       el: ".swiper-scrollbar",
+//       hide: false,
+//     },
+// });
+
+// var swiper = new Swiper(".mySwiper", {
+//     cssMode: true,
+//     loop: true,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+//   });
+
+
+
+//slider paiin
+
+
+  
+
+
+ 
+  
